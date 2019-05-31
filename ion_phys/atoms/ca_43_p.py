f653d2e24d35a7e1f41730a323cff25e58840442
RHanley1 <ryan.hanley@physics.ox.ac.uk>
2019-05-31 09:47:56 +0100

    Added P3/2, D3/2 and D5/2 levels.

--- ion_phys/atoms/ca_43_p.py
+++ ion_phys/atoms/ca_43_p.py
@@ -6,6 +6,7 @@ References:
 [2] - G. Tommaseo, et al., The European Physical Journal D, 25 (2003)
 [3] - T. P. Harty, et al. Phys. Rev. Lett 113, 220501 (2014)
 [4] - W.  Nortershauser, et al., The European Physical Journal D, 2 (1998)
+[5] - J. Benhelm, et al., PHYSICAL REVIEW A 75, 032506 (2007)
 """
 
 import scipy.constants as consts
@@ -22,6 +23,21 @@ atom["levels"] = {
     },
     Level(n=4, L=1, S=1/2, J=1/2): {
         "Ahfs": -145.4e6 * consts.h,  # [4]
+        "gI": (2 / 7) * -1.315348  # [3]
+    },
+    Level(n=4, L=1, S=1/2, J=3/2): {
+        "Ahfs": -31.4e6 * consts.h,  # [4]
+        "Bhfs": -6.9 * consts.h,  # [4]
+        "gI": (2 / 7) * -1.315348  # [3]
+    },    
+    Level(n=4, L=2, S=1/2, J=3/2): {
+        "Ahfs": -47.3e6 * consts.h,  # [4]
+        "Bhfs": -3.7 * consts.h,  # [4]
+        "gI": (2 / 7) * -1.315348  # [3]
+    },
+        Level(n=4, L=2, S=1/2, J=5/2): {
+        "Ahfs": -3.8931e6 * consts.h,  # [5]
+        "Bhfs": 4.241 * consts.h,  # [5]
         "gI": (2 / 7) * -1.315348  # [3]
     }
 }
