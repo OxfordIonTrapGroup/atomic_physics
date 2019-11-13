16b041ecdac932dc45638082980c9f9d64262413
Ryan Hanley <ryan.hanley@physics.ox.ac.uk>
2019-11-13 11:39:14 +0000

    Function to calculate AC Zeeman shifts
    I have added a function which returns the AC Zeeman shift
    of a particular statein units of (Hz/T^2).

--- ion_phys/ground_level.py
+++ ion_phys/ground_level.py
@@ -8,7 +8,7 @@ def transition_freq(B, atom, level, lower, upper):
     """ Returns the frequency of a transition between two states in the same
     level at a given magnetic field.
 
-    :param B: the field
+    :param B: the static field (T)
     :param atom: the atom to work with
     :param level: the level the states are in
     :param lower: (F, MF) tuple for the lower-energy state
@@ -30,7 +30,7 @@ def df_dB(B, atom, level, lower, upper, eps=1e-4):
     """ Returns the field-sensitivity (Hz/T) of a transition between two states
     in the same level at a given magnetic field.
 
-    :param B: the field
+    :param B: the static field (T)
     :param atom: the atom to work with
     :param level: the level the states are in
     :param lower: (F, MF) tuple for the lower-energy state
@@ -47,7 +47,7 @@ def d2f_dB2(B, atom, level, lower, upper, eps=1e-4):
     """ Returns the second-order field-sensitivity (Hz/T^2) of a transition
     between two states in the same level at a given magnetic field.
 
-    :param B: the field
+    :param B: the static field (T)
     :param atom: the atom to work with
     :param level: the level the states are in
     :param lower: (F, MF) tuple for the lower-energy state
@@ -76,3 +76,36 @@ def field_insensitive_point(atom, level, lower, upper, B_min=1e-3, B_max=1e-1):
                    options={'xtol': 1e-4, 'eps': 1e-7})
 
     return res.x[0] if res.success else None
+
+
+def ac_zeeman_shift(B, atom, level, state, freq):
+    """ Returns the AC Zeeman shift of a particular state (Hz/T^2).
+    :param B: the static field (T)
+    :param atom: the atom to work with
+    :param level: the level the states are in
+    :param state: (F, MF) tuple of the state which the
+        AC Zeeman shift is calculated
+    :param freq: frequency of the driving field (Hz)
+    :return: Array of AC Zeeman shift (Hz) caused by sigma_minus,
+        pi and sigma_plus polarisations [sigma_m, pi, sigma_p]
+    """
+    ip.init(atom, B)
+    ip.calc_m1(atom)
+
+    M = level["M"]
+    F = level["F"]
+    R = level["R_m1"]
+    rabi_freq = R/consts.hbar
+
+    zm_shift = np.zeros(3)
+    for f in np.unique(level["F"]):
+        for m in np.arange(state[1] - 1, state[1] + 2):
+            if abs(m) > f:
+                continue
+            else:
+                q = m - state[1]
+                trans_freq = 2*np.pi*transition_freq(B, atom, level, state, (f, m))
+                w = rabi_freq[np.logical_and(F == state[0], M == state[1]),
+                              np.logical_and(F == f, M == m)][0]
+                zm_shift[q + 1] += 0.5*w**2*(trans_freq/(trans_freq**2 - (2*np.pi*freq)**2))
+    return zm_shift/(2*np.pi)
