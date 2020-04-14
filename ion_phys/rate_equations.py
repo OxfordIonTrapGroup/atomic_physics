fix spontaneous scattering rate calculation and test in high/low fields

ion_phys/rate_equations.py
@@ -1,6 +1,7 @@
 import numpy as np
 from .wigner import wigner3j
 
+
 class Rates:
     def __init__(self, ion):
         self.ion = ion
@@ -10,10 +11,12 @@ class Rates:
 
     def get_spont(self):
         """ Returns the spontaneous emission matrix. """
-        spont = np.zeros((self.ion.num_states, self.ion.num_states))
+        # TODO: express the spontaneous rates in terms of the multipole matrix
+        # elements and move that calculation into Ion
         ion = self.ion
-        I_dim = np.rint(2.0*ion.I+1).astype(int)
-        for _, transition in ion.transitions.items():
+        spont = np.zeros((self.ion.num_states, self.ion.num_states))
+        Idim = np.rint(2.0*ion.I+1).astype(int)
+        for name, transition in ion.transitions.items():
             A = transition.A
             upper = transition.upper
             lower = transition.lower
@@ -21,120 +24,39 @@ class Rates:
             Jl = lower.J
             Mu = np.arange(-Ju, Ju+1)
             Ml = np.arange(-Jl, Jl+1)
-            jdim_u = int(np.rint(2*Ju+1))
-            jdim_l = int(np.rint(2*Jl+1))
-            jdim = jdim_u + jdim_l
+            Jdim_u = int(np.rint(2*Ju+1))
+            Jdim_l = int(np.rint(2*Jl+1))
+            Jdim = Jdim_u + Jdim_l
+
+            order = Ju-Jl
+            if order > 1:
+                print("skipping {}".format(order))
+                continue
 
             # calculate scattering rates in the high-field basis so we can
             # forget about nuclear spin
-            Gamma_hf = np.zeros((jdim, jdim))
-            for i_u in range(jdim_u):
+            Gamma_hf = np.zeros((Jdim, Jdim))
+            for ind_u in range(Jdim_u):
+                # q = Ml - Mu
                 for q in [-1, 0, 1]:
-                    if abs(Mu[i_u] + q) > Jl:
+                    if abs(Mu[ind_u] + q) > Jl:
                         continue
-                    i_l = np.argwhere(Ml == Mu[i_u]+q)
-                    # Gamma_hf[i_l, i_u+jdim_l] = wigner3j(
-                    #     Jl, 1, Ju, -(Mu[i_u]+q), q, Mu[i_u])**2
-                    Gamma_hf[i_l, i_u+jdim_l] = wigner3j(
-                        Jl, 1, Ju, -(Mu[i_u]+q), q, Mu[i_u])
+                    ind_l = np.argwhere(Ml == Mu[ind_u]+q)
+                    sign = (-1)**(-Mu[ind_u]+Jl+1)
+                    Gamma_hf[ind_l, ind_u+Jdim_l] = wigner3j(
+                        Jl, 1, Ju, -(Mu[ind_u]+q), q, Mu[ind_u])*sign
             Gamma_hf *= np.sqrt(A*(2*Ju+1))
 
             # introduce the (still decoupled) nuclear spin
-            Gamma_hf = np.kron(Gamma_hf, np.identity(I_dim))
+            Gamma_hf = np.kron(Gamma_hf, np.identity(Idim))
 
             # now couple...
-            M = np.concatenate((ion.M[ion.slice(lower)],
-                                ion.M[ion.slice(upper)]))
             subspace = np.r_[ion.slice(lower), ion.slice(upper)]
-            num = len(M)
-            Gamma = np.zeros(Gamma_hf.shape)
-            VV = np.zeros((num, num))
-            for ii in range(num):
-                for jj in range(num):
-                    VV[ii, jj] = (ion.V[subspace[ii], subspace[jj]])
+            subspace = np.ix_(subspace, subspace)
+            V = ion.V[subspace]
+            spont[subspace] = np.power(np.abs((V.T)@Gamma_hf@(V)), 2)
 
-            # conclusion: there seems to be a sign missing here that
-            # would lead to some interference between terms...need to look
-            # at the physics next...
-            Gamma = np.power(np.abs((VV.T)@Gamma_hf@(VV)),2)
-            # print(VV[1+16,17]**2*8)
-            # print(np.power(VV[16:,17],2)*8)
-            print(np.min(np.min(Gamma_hf)))
-            # this would make sense if there were a sign in the scattering
-            # amplitude...
-            # print(Gamma_hf[1, 16:])
-            # print("ho")
-            # print((VV.T@Gamma_hf)[1,16:])
-            # print(Gamma[1,16:])
-
-            # # low-field test...
-            I = ion.I
-            from sympy.physics.wigner import wigner_6j
-            M = np.concatenate((ion.M[ion.slice(lower)],
-                                ion.M[ion.slice(upper)]))
-            F = np.concatenate((ion.F[ion.slice(lower)],
-                                ion.F[ion.slice(upper)]))
-            test = np.zeros((len(M), len(M)))
-            l_dim = len(ion.M[ion.slice(lower)])
-            u_dim = len(ion.M[ion.slice(upper)])
-            for i in range(l_dim):
-                for j in range(u_dim):
-                    ind_i = i
-                    ind_j = j + l_dim
-                    M_i = M[ind_i]
-                    M_j = M[ind_j]
-                    F_i = F[ind_i]
-                    F_j = F[ind_j]
-                    q = M_i - M_j
-                    test[ind_i, ind_j] = A*(
-                        (2*Ju+1)*(2*F_i+1)*(2*F_j+1)
-                        *(wigner3j(F_i, 1, F_j, -M_i, q, M_j))**2
-                        *(wigner_6j(Jl, I, F_i, F_j, 1, Ju)**2))
-            from pprint import pprint
-            # print("test")
-            # pprint(test[1, 16:])
-            # # pprint((test-Gamma)[1, :])
-            # pprint("F: {}".format(F[16:]))
-            # pprint("MF: {}".format(M[16:]))
-            pprint("mi ax {}".format(ion.MIax[subspace][:16]))
-            pprint("mj ax {}".format(ion.MJax[subspace][:16]))
-            # pprint("gamma")
-            # pprint(Gamma_hf[0, 16:])
-            # pprint(test[1, 17])
-            # pprint(Gamma[1, 17])
-            # pprint("error {}:".format(np.max(np.max(np.abs(test-Gamma)))))
-
-            # high-field test...
-            # M = np.concatenate((ion.M[ion.slice(lower)],
-            #                     ion.M[ion.slice(upper)]))
-            # MI = np.concatenate((ion.MI[ion.slice(lower)],
-            #                      ion.MI[ion.slice(upper)]))
-            # MJ = np.concatenate((ion.MJ[ion.slice(lower)],
-            #                      ion.MJ[ion.slice(upper)]))
-            # test = np.zeros((len(M), len(M)))
-            # l_dim = len(ion.M[ion.slice(lower)])
-            # u_dim = len(ion.M[ion.slice(upper)])
-            # print(l_dim, u_dim)
-            # for i in range(l_dim):
-            #     for j in range(u_dim):
-            #         ind_i = i
-            #         ind_j = j + l_dim
-            #         if MI[ind_i] != MI[ind_j]:
-            #             continue
-            #         M_i = MJ[ind_i]
-            #         M_j = MJ[ind_j]
-            #         q = M_i - M_j
-            #         if q not in [-1, 1, 0]:
-            #             continue
-            #         test[ind_i, ind_j] = A*(2*Ju+1)*(wigner3j(Jl, 1, Ju, -M_i, q, M_j))**2
-            # from pprint import pprint
-            # pprint(test)
-            # pprint(Gamma_hf)
-
-            # pprint("err: {}".format(np.max(np.max(np.abs(test-Gamma_hf)))))
-
-            return Gamma_hf
-
+        return spont
 
     def get_stim(self, lasers):
         """ Returns the stimulated emission matrix for a list of lasers. """

ion_phys/tests/test_spont.py
@@ -1,17 +1,94 @@
 import numpy as np
 import unittest
+from sympy.physics.wigner import wigner_3j, wigner_6j
 from ion_phys.ions.ca43 import Ca43
 from ion_phys.rate_equations import Rates
-from pprint import pprint
+
 
 class TestSpont(unittest.TestCase):
-    def test_spont(self):
-        """ ...
-        """
+    def test_LF(self):
+        """ Check that, in the low-field, our spontaneous scattering rates
+        match a more direct calculation. """
         ion = Ca43(1e-8)
         rates = Rates(ion)
-        rates.get_spont()
+        I = ion.I
+        Idim = int(np.rint(2*I+1))
+        spont_rates = rates.get_spont()
+        spont = np.zeros((ion.num_states, ion.num_states))
 
+        for name, transition in ion.transitions.items():
+            A = transition.A
+            lower = transition.lower
+            upper = transition.upper
+            Ju = upper.J
+            Jl = lower.J
+            Jdim_u = int(np.rint(2*Ju+1))
+            Jdim_l = int(np.rint(2*Jl+1))
+            l_dim = Idim*Jdim_l
+            u_dim = Idim*Jdim_u
+
+            subspace = np.r_[ion.slice(lower), ion.slice(upper)]
+            num_states = len(subspace)
+
+            for l_ind in list(subspace[:l_dim]):
+                for u_ind in list(subspace[l_dim:]):
+                    Fl = ion.F[l_ind]
+                    Fu = ion.F[u_ind]
+                    Ml = ion.M[l_ind]
+                    Mu = ion.M[u_ind]
+                    q = Ml - Mu
+                    if q not in [-1, 1, 0]:
+                        continue
+
+                    spont[l_ind, u_ind] = A*(
+                        (2*Ju+1)
+                        *(2*Fl+1)
+                        *(2*Fu+1)
+                        *(wigner_3j(Fl, 1, Fu, -Ml, q, Mu))**2
+                        *(wigner_6j(Jl, I, Fl, Fu, 1, Ju)**2))
+
+        scale = np.max(np.max(np.abs(spont)))
+        eps = np.max(np.max(np.abs(spont-spont_rates)))
+        spont = spont[np.ix_(subspace, subspace)]
+        self.assertTrue(eps/scale < 1e-4)
+
+    def test_HF(self):
+        """ Check that, in the high-field, our spontaneous scattering rates
+        match a more direct calculation. """
+        ion = Ca43(1000)
+        rates = Rates(ion)
+        Idim = int(np.rint(2*ion.I+1))
+        spont_rates = rates.get_spont()
+        spont = np.zeros((ion.num_states, ion.num_states))
+
+        for _, transition in ion.transitions.items():
+            A = transition.A
+            lower = transition.lower
+            upper = transition.upper
+            Ju = upper.J
+            Jl = lower.J
+            Jdim_u = int(np.rint(2*Ju+1))
+            Jdim_l = int(np.rint(2*Jl+1))
+            l_dim = Idim*Jdim_l
+            u_dim = Idim*Jdim_u
+
+            subspace = np.r_[ion.slice(lower), ion.slice(upper)]
+            num_states = len(subspace)
+
+            for l_ind in list(subspace[:l_dim]):
+                for u_ind in list(subspace[l_dim:]):
+                    if ion.MI[l_ind] != ion.MI[u_ind]:
+                        continue
+                    M_l = ion.MJ[l_ind]
+                    M_u = ion.MJ[u_ind]
+                    q = M_l - M_u
+                    if q not in [-1, 1, 0]:
+                        continue
+                    spont[l_ind, u_ind] = A*(2*Ju+1)*(
+                        wigner_3j(Jl, 1, Ju, -M_l, q, M_u))**2
+        scale = np.max(np.max(np.abs(spont)))
+        eps = np.max(np.max(np.abs(spont-spont_rates)))
+        self.assertTrue(eps/scale < 1e-8)
 
 
 if __name__ == '__main__':