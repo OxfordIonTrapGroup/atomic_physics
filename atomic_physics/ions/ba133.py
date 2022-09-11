64e14e9b9bca47cdfd43bca6bed890bc46069966
Ana Sotirova <ana.sotirova@balliol.ox.ac.uk>
2022-09-11 21:36:20 +0100

    ions/ba133: add isotope shifts to transition frequencies

--- atomic_physics/ions/ba133.py
+++ atomic_physics/ions/ba133.py
@@ -6,6 +6,13 @@ A cursory look turned up:
 However, this data is based of calculations done in the late 60s. These are
 only ~10% accurate.
 
+The transition frequencies are calculated based on the 138Ba+ frequencies
+from [1] and the isotope shifts in the second reference listed next to
+the freqiency. Where no references are given, the transition frequencies
+were calculated based on transition frequencies between other levels.
+For example the frequency of the 1762 nm transition f(1762) was found
+from f(1762)=f(455)-f(614).
+
 References:
 [1] - A. Kramida, NIST Atomic Spectra Database (ver. 5.9) (2021)
 [2] - A. A. Madej and J. D. Sankey, Phys. Rev. A 41, 2621, (1990)
@@ -76,43 +83,43 @@ class Ba133(ap.Atom):
                 lower=S12,
                 upper=P12,
                 A=9.53e7,  # [1]
-                freq=2 * np.pi * 607426317510693.9,  #
+                freq=2 * np.pi * 607426317511066.9,  # [1], [6]
             ),
             "455": ap.Transition(
                 lower=S12,
                 upper=P32,
                 A=1.11e8,  # [1]
-                freq=2 * np.pi * 658116515416903.1,  #
+                freq=2 * np.pi * 658116515417261.1,  # [1], [7]
             ),
             "650": ap.Transition(
                 lower=D32,
                 upper=P12,
                 A=3.1e7,  # [1]
-                freq=2 * np.pi * 461311910409872.25,  #
+                freq=2 * np.pi * 461311910410070.25,  # [1], [6]
             ),
             "585": ap.Transition(
                 lower=D32,
                 upper=P32,
                 A=6.0e6,  # [1]
-                freq=2 * np.pi * 512002108316081.56,  #
+                freq=2 * np.pi * 512002108316264.5,
             ),
             "614": ap.Transition(
                 lower=D52,
                 upper=P32,
                 A=4.12e7,  # [1]
-                freq=2 * np.pi * 487990081496342.56,  #
+                freq=2 * np.pi * 487990081496558.56,  # [1], [7]
             ),
             "1762": ap.Transition(
                 lower=S12,
                 upper=D52,
                 A=29e-3,  # [2]
-                freq=2 * np.pi * 170126433920560.6,  #
+                freq=2 * np.pi * 170126433920702.56,
             ),
             "2051": ap.Transition(
                 lower=S12,
                 upper=D32,
                 A=12.5e-3,  # [3]
-                freq=2 * np.pi * 146114407100821.6,  #
+                freq=2 * np.pi * 146114407100996.62,
             ),
         }
 
