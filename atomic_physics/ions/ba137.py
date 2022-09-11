db88528ff1eaa680a981d1edab3b7562cd4884f5
Ana Sotirova <ana.sotirova@balliol.ox.ac.uk>
2022-09-11 21:50:54 +0100

    ions/ba137: add isotope shifts to transition frequencies

--- atomic_physics/ions/ba137.py
+++ atomic_physics/ions/ba137.py
@@ -6,6 +6,13 @@ A cursory look turned up:
 However, this data is based of calculations done in the late 60s. These are
 only ~10% accurate.
 
+The transition frequencies are calculated based on the 138Ba+ frequencies
+from [1] and the isotope shifts in the second reference listed next to
+the freqiency. Where no references are given, the transition frequencies
+were calculated based on transition frequencies between other levels.
+For example the frequency of the 1762 nm transition f(1762) was found
+from f(1762)=f(455)-f(614).
+
 References:
 [1] - A. Kramida, NIST Atomic Spectra Database (ver. 5.9) (2021)
 [2] - A. A. Madej and J. D. Sankey, Phys. Rev. A 41, 2621 (1990)
@@ -14,13 +21,16 @@ References:
   quadrupole moments, Atomic Data and Nuclear Data Tables, Volume 90,
   Issue 1 (2005)
 [5] - H. Knab, K. H. Knöll, F. Scheerer and G. Werth, Zeitschrift für
-  Physik D Atoms, Molecules and Clusters volume 25, pages205–208 (1993)
+  Physik D Atoms, Molecules and Clusters volume 25, pages 205–208 (1993)
 [6] - R. Blatt and G. Werth, Phys. Rev. A 25, 1476 (1982)
 [7] - P Villemoes et al, J. Phys. B: At. Mol. Opt. Phys. 26 4289 (1993)
 [8] - Nicholas C. Lewty, Boon Leng Chuah, Radu Cazan, B. K. Sahoo, and
   M. D. Barrett, Opt. Express 21, 7131-7132 (2013)
 [9] - Nicholas C. Lewty, Boon Leng Chuah, Radu Cazan, Murray D. Barrett,
   and B. K. Sahoo, Phys. Rev. A 88, 012518 (2013)
+[10] - K. Wendt, S. A. Ahmad, F. Buchinger, A. C. Mueller, R. Neugart, and
+  E. -W. Otten, Zeitschrift für Physik A Atoms and Nuclei volume 318,
+  pages 125–129 (1984)
 """
 import numpy as np
 import typing
@@ -80,43 +90,43 @@ class Ba137(ap.Atom):
                 lower=S12,
                 upper=P12,
                 A=9.53e7,  # [1]
-                freq=2 * np.pi * 607426317510693.9,  #
+                freq=2 * np.pi * 607426317510965.0,  # [1], [10]
             ),
             "455": ap.Transition(
                 lower=S12,
                 upper=P32,
                 A=1.11e8,  # [1]
-                freq=2 * np.pi * 658116515416903.1,  #
+                freq=2 * np.pi * 658116515417166.6,
             ),
             "650": ap.Transition(
                 lower=D32,
                 upper=P12,
                 A=3.1e7,  # [1]
-                freq=2 * np.pi * 461311910409872.25,  #
+                freq=2 * np.pi * 461311910409885.25,  # [1], [7]
             ),
             "585": ap.Transition(
                 lower=D32,
                 upper=P32,
                 A=6.0e6,  # [1]
-                freq=2 * np.pi * 512002108316081.56,  #
+                freq=2 * np.pi * 512002108316086.9,  # [1], [7]
             ),
             "614": ap.Transition(
                 lower=D52,
                 upper=P32,
                 A=4.12e7,  # [1]
-                freq=2 * np.pi * 487990081496342.56,  #
+                freq=2 * np.pi * 487990081496344.9,  # [1], [7]
             ),
             "1762": ap.Transition(
                 lower=S12,
                 upper=D52,
                 A=29e-3,  # [2]
-                freq=2 * np.pi * 170126433920560.6,  #
+                freq=2 * np.pi * 170126433920558.25,
             ),
             "2051": ap.Transition(
                 lower=S12,
                 upper=D32,
                 A=12.5e-3,  # [3]
-                freq=2 * np.pi * 146114407100821.6,  #
+                freq=2 * np.pi * 146114407101079.75,
             ),
         }
 
