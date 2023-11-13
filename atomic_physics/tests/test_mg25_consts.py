893f9bf32240f51022e0c49025091b7150e24361
mollysmith23 <molly.smith@physics.ox.ac.uk>
2023-11-13 17:34:14 +0000

    Update test_mg25_consts.py

--- atomic_physics/tests/test_mg25_consts.py
+++ atomic_physics/tests/test_mg25_consts.py
@@ -10,15 +10,15 @@ from atomic_physics.utils import field_insensitive_point
 import numpy as np
 
 S12_31_21_clock_field = 212.8e-4  # [1]
-MW_transition_freqs = {
-    (3, 2): 1.326456e9,
-    (2, 2): 1.460516e9,
-    (1, 2): 1.573543e9,
-    (2, 1): 1.573432e9,
-    (1, 1): 1.686459e9,
-    (0, 1): 1.786044e9,
-    (0, -1): 1.975445e9,
-}  # [1]
+MW_transition_freqs = [
+    [3, 2, 1.326456e9],
+    [2, 2, 1.460516e9],
+    [1, 2, 1.573543e9],
+    [2, 1, 1.573432e9],
+    [1, 1, 1.686459e9],
+    [0, 1, 1.786044e9],
+    [0, -1, 1.975445e9],
+    ]  # [1]
 
 
 class TestMg25Consts(unittest.TestCase):
@@ -53,14 +53,14 @@ class TestMg25Consts(unittest.TestCase):
         )
         ion.setB(model_field_independent_point)
 
-        for key, value in MW_transition_freqs.items():
+        for transition in MW_transition_freqs:
             freq_model = (
                 ion.delta(
-                    ion.index(mg25.S12, F=3, M=key[0]),
-                    ion.index(mg25.S12, F=2, M=key[1]),
+                    ion.index(mg25.S12, F=3, M=transition[0]),
+                    ion.index(mg25.S12, F=2, M=transition[1]),
                 )
                 / 2
                 * np.pi
             )
 
-            self.assertAlmostEqual(freq_model, value, places=4)
+            self.assertAlmostEqual(freq_model, transition[3], places=4)
