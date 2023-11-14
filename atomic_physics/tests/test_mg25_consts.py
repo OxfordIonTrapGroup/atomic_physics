test_mg25_consts - fixed factor 2 pi

atomic_physics/tests/test_mg25_consts.py
@@ -44,7 +44,7 @@ class TestMg25Consts(unittest.TestCase):
         """
         ion = mg25.Mg25(level_filter=[mg25.S12])
 
-        ion.setB(S12_31_21_clock_field)
+        ion.setB(1e-4)
         l_index = ion.index(mg25.S12, 1, F=3)
         u_index = ion.index(mg25.S12, 1, F=2)
 
@@ -54,13 +54,9 @@ class TestMg25Consts(unittest.TestCase):
         ion.setB(model_field_independent_point)
 
         for transition in MW_transition_freqs:
-            freq_model = (
-                ion.delta(
-                    ion.index(mg25.S12, F=3, M=transition[0]),
-                    ion.index(mg25.S12, F=2, M=transition[1]),
-                )
-                / 2
-                * np.pi
-            )
+            freq_model = ion.delta(
+                ion.index(mg25.S12, F=3, M=transition[0]),
+                ion.index(mg25.S12, F=2, M=transition[1]),
+            ) / (2 * np.pi)
 
             self.assertAlmostEqual(freq_model, transition[2], places=4)