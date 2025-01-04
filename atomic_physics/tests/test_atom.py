a1093b3c5e1556cd5bb5e78c0e8503da312fb9bb
Thomas Harty <thomas.peter.harty@gmail.com>
2025-01-04 23:52:17 +0000

    tests: expand test_atom

--- atomic_physics/tests/test_atom.py
+++ atomic_physics/tests/test_atom.py
@@ -6,6 +6,7 @@ from scipy import constants as consts
 from atomic_physics import operators
 from atomic_physics.core import AtomFactory, Level, LevelData
 from atomic_physics.ions import ba133, ba137, ca43
+from atomic_physics.operators import expectation_value
 
 
 class TestAtom(unittest.TestCase):
@@ -13,12 +14,34 @@ class TestAtom(unittest.TestCase):
 
     References:
         [1] Thomas Harty DPhil Thesis (2013).
+        [2] David Szwer DPhil Thesis (2009)
     """
+
+    def test_num_states(self):
+        """Check that the ``num_states`` is calculated correctly and that all attributes
+        have the correct shape.
+        """
+        ion = ca43.Ca43(magnetic_field=146.0942e-4)
+
+        num_states = 0
+        for level in ion.level_data.keys():
+            num_states += (2 * ion.nuclear_spin + 1) * (2 * level.J + 1)
+        assert num_states == ion.num_states
+
+        assert ion.num_states == num_states
+        assert ion.state_energies.shape == (num_states,)
+        assert ion.state_vectors.shape == (num_states, num_states)
+        assert ion.high_field_M_I.shape == (num_states,)
+        assert ion.high_field_M_J.shape == (num_states,)
+        assert ion.M.shape == (num_states,)
+        assert ion.F.shape == (num_states,)
+        assert ion.M_I.shape == (num_states,)
+        assert ion.M_J.shape == (num_states,)
 
     def test_state_vectors(self):
         """
-        Check the calculation for `state_vectors` against values for the ground-level of
-        43Ca+ at 146G using [1] table E.2.
+        Check the calculation for ``state_vectors`` against values for the ground-level
+        of 43Ca+ at 146G using [1] table E.2.
         """
         ion = ca43.Ca43(magnetic_field=146.0942e-4)
         level_slice = ion.get_slice_for_level(ca43.ground_level)
@@ -98,6 +121,57 @@ class TestAtom(unittest.TestCase):
             matched[-1, :] = True
 
         assert np.all(matched)
+
+    def test_state_energies(self):
+        """
+        Check the calculation for ``state_energies`` against values for the ground-level
+        transitions within 43Ca+ at 146G using [1] table E.2.
+
+        This also checks the behaviour of ``get_transition_for_levels``.
+        """
+        ion = ca43.Ca43(magnetic_field=146.0942e-4)
+
+        # M_4, M_3, frequency
+        ref = (
+            (-4, -3, 3.589033180),
+            (-3, -3, 3.543000725),
+            (-3, -2, 3.495825733),
+            (-2, -3, 3.4957420317),
+            (-2, -2, 3.448567039),
+            (-2, -1, 3.4000621586),
+            (-1, -2, 3.399978455),
+            (-1, -1, 3.351473573),
+            (-1, 0, 3.301519669),
+            (0, -1, 3.301435966),
+            (0, 0, 3.2514820620),
+            (0, +1, 3.199941077),
+            (+1, 0, 3.199857374),
+            (+1, +1, 3.148316389),
+            (+1, +2, 3.095026842),
+            (+2, +1, 3.094943139),
+            (+2, +2, 3.041653592),
+            (+2, +3, 2.986424605),
+            (+3, +2, 2.986340903),
+            (+3, +3, 2.931111915),
+            (+4, +3, 2.873631427),
+        )
+        for M_4, M_3, ref_frequency in ref:
+            F_4_ind = ion.get_state_for_F(level=ca43.ground_level, F=4, M_F=M_4)
+            F_3_ind = ion.get_state_for_F(level=ca43.ground_level, F=3, M_F=M_3)
+
+            np.testing.assert_allclose(
+                (ion.state_energies[F_3_ind] - ion.state_energies[F_4_ind])
+                / (2 * np.pi),
+                ref_frequency * 1e9,
+                atol=1,
+            )
+
+            np.testing.assert_allclose(
+                ion.get_transition_frequency_for_states(states=(F_4_ind, F_3_ind))
+                / (2 * np.pi),
+                ref_frequency * 1e9,
+                atol=1,
+            )
 
     def test_magnetic_dipoles(self):
         """
@@ -281,44 +355,44 @@ class TestAtom(unittest.TestCase):
             factory = AtomFactory(
                 level_data=(level_data,), transitions=tuple(), nuclear_spin=nuclear_spin
             )
-            # level = ca43.ground_level
-            # factory = ca43.Ca43.filter_levels(level_filter=(level, ))
             atom = factory(magnetic_field=1e-9)
-
-            # nuclear_spin = atom.nuclear_spin
 
             # In the low field we should have <F^2> = F(F+1)
             I_dim = int(np.rint(2 * nuclear_spin + 1))
             J_dim = int(np.rint(2 * level.J + 1))
 
-            Jp = np.kron(operators.Jp(level.J), np.identity(I_dim))
-            Jm = np.kron(operators.Jm(level.J), np.identity(I_dim))
-            Jz = np.kron(operators.Jz(level.J), np.identity(I_dim))
+            J_p = -1 / np.sqrt(2) * np.kron(operators.Jp(level.J), np.identity(I_dim))
+            J_m = +1 / np.sqrt(2) * np.kron(operators.Jm(level.J), np.identity(I_dim))
+            J_z = np.kron(operators.Jz(level.J), np.identity(I_dim))
 
-            Ip = np.kron(np.identity(J_dim), operators.Jp(atom.nuclear_spin))
-            Im = np.kron(np.identity(J_dim), operators.Jm(atom.nuclear_spin))
-            Iz = np.kron(np.identity(J_dim), operators.Jz(atom.nuclear_spin))
+            I_p = (
+                -1
+                / np.sqrt(2)
+                * np.kron(np.identity(J_dim), operators.Jp(atom.nuclear_spin))
+            )
+            I_m = (
+                +1
+                / np.sqrt(2)
+                * np.kron(np.identity(J_dim), operators.Jm(atom.nuclear_spin))
+            )
+            I_z = np.kron(np.identity(J_dim), operators.Jz(atom.nuclear_spin))
 
-            Fz = Iz + Jz
-            Fp = Ip + Jp
-            Fm = Im + Jm
+            F_z = I_z + J_z
+            F_p = I_p + J_p
+            F_m = I_m + J_m
 
-            F_2_op = Fz @ Fz + (1 / 2) * (Fp @ Fm + Fm @ Fp)
-            F_2 = np.diag(
-                atom.state_vectors.conj().T @ F_2_op @ atom.state_vectors
-            )  # <F^2>
-
+            F_2_op = F_z @ F_z - (F_p @ F_m + F_m @ F_p)
+            F_2 = expectation_value(atom.state_vectors, F_2_op)  # <F^2>
             F = 0.5 * (np.sqrt(1 + 4 * F_2) - 1)  # <F^2> = f * (f + 1)
+
             np.testing.assert_allclose(atom.F, F, atol=0.1)
 
     def test_M_I_M_J(self):
-        """Check ordering of M_I and M_J by looking at the ground level of
-        Ba 137.
-        """
-        level = ba137.ground_level
-        Ba137 = ba137.Ba137.filter_levels(level_filter=(level,))
-        ion = Ba137(magnetic_field=1.0)
+        """Check ordering of M_I and M_J by looking at level structure of 137Ba+."""
+        ion = ba137.Ba137(magnetic_field=10.0)
 
+        # Start by checking states in the ground level against their known ordering
+        level = ba137.ground_level
         self.assertEqual(
             ion.get_state_for_F(
                 level,
@@ -383,3 +457,118 @@ class TestAtom(unittest.TestCase):
             ),
             ion.get_state_for_MI_MJ(level, M_I=3 / 2, M_J=-1 / 2),
         )
+
+        # now check that all values match against the expectation values of the relevant
+        # operators
+        for level in ion.level_data.keys():
+            level_slice = ion.get_slice_for_level(level)
+            state_vectors = ion.state_vectors[level_slice, level_slice]
+            I_dim = int(np.rint(2 * ion.nuclear_spin + 1))
+            J_dim = int(np.rint(2 * level.J + 1))
+
+            J_z = np.kron(operators.Jz(level.J), np.identity(I_dim))
+            I_z = np.kron(np.identity(J_dim), operators.Jz(ion.nuclear_spin))
+
+            M_J = expectation_value(state_vectors=state_vectors, operator=J_z)
+            M_I = expectation_value(state_vectors=state_vectors, operator=I_z)
+
+            np.testing.assert_allclose(M_J, ion.M_J[level_slice], atol=1e-2)
+            np.testing.assert_allclose(M_I, ion.M_I[level_slice], atol=1e-2)
+
+    def test_get_slice_for_level(self):
+        """Test for ``get_slice_for_level``."""
+        ion = ca43.Ca43(1)
+
+        # check that all states are included in at exactly one slice
+        all_states = np.full(ion.num_states, False, dtype=bool)
+        for level in ion.level_data.keys():
+            level_slice = ion.get_slice_for_level(level)
+            assert not any(all_states[level_slice])
+            all_states[level_slice] = True
+        assert all(all_states)
+
+        # check that each slice has the correct number of elements
+        for level in ion.level_data.keys():
+            num_states = (2 * ion.nuclear_spin + 1) * (2 * level.J + 1)
+            level_slice = ion.get_slice_for_level(level)
+            assert level_slice.stop - level_slice.start == num_states
+
+    def get_level_for_state(self):
+        """Test for ``get_level_for_state``."""
+        ion = ca43.Ca43(1)
+        for state in range(ion.num_states):
+            level = ion.get_level_for_state(state)
+            assert state >= level.start
+            assert state < level.stop
+
+    def get_transition_for_levels(self):
+        """Test for ``get_transition_for_levels``."""
+        ion = ca43.Ca43(1)
+        for transition in ion.transitions:
+            assert (
+                ion.get_transition_for_levels((transition.lower, transition.upper))
+                == transition
+            )
+            assert (
+                ion.get_transition_for_levels((transition.upper, transition.lower))
+                == transition
+            )
+
+    def test_get_rabi_m1(self):
+        """Test for ``get_rabi_m1``."""
+        ion = ca43.Ca43(magnetic_field=146e-4)
+        level = ca43.ground_level
+        level_slice = ion.get_slice_for_level(level)
+        mpoles = ion.get_magnetic_dipoles()
+        for ii in range(level_slice.start, level_slice.stop):
+            for jj in range(level_slice.start, level_slice.stop):
+                Omega = ion.get_rabi_m1(lower=jj, upper=ii, amplitude=1)
+                np.testing.assert_allclose(Omega, mpoles[ii, jj] / consts.hbar)
+
+    def test_get_states_for_M(self):
+        """Test for ``get_states_for_M``."""
+        ion = ca43.Ca43(magnetic_field=146e-4)
+        for level in ion.level_data.keys():
+            F_min = np.abs(ion.nuclear_spin - level.J)
+            F_max = ion.nuclear_spin + level.J
+            level_F = np.arange(F_min, F_max + 1)
+            for M in np.arange(-F_max, F_max + 1):
+                M_states = ion.get_states_for_M(level=level, M=M)
+                assert len(M_states) == len(set(M_states))
+                M_F_states = set(
+                    [
+                        ion.get_state_for_F(level=level, F=F, M_F=M)
+                        for F in level_F[level_F >= np.abs(M)]
+                    ]
+                )
+                assert set(M_states) == M_F_states
+
+    def test_get_saturation_intensity(self):
+        """Test for ``get_saturation_intensity``."""
+        ion = ca43.Ca43(1)
+
+        # saturation intensities for 43Ca+ transitions from [2]
+        # units are Wm^-2
+        ref = (
+            ("397", 933.82),
+            ("393", 987.58),
+            ("866", 89.798),
+            ("850", 97.954),
+            ("854", 96.446),
+            # ("729", 9.181e-7), # FIXME #83
+        )
+        for transition, I_sat_ref in ref:
+            I_sat = ion.get_saturation_intensity(transition)
+            np.testing.assert_allclose(I_sat, I_sat_ref, 1e-2)
+
+    def test_intensity_to_power(self):
+        """Test for ``intensity_to_power``."""
+        ion = ca43.Ca43(1)
+        waist = 10e-6
+        power = ion.intensity_to_power(
+            transition="397",
+            waist_radius=waist,
+            intensity=1,
+        )
+        # Isat = 933.82 [2]
+        np.testing.assert_allclose(power, 1 / 2 * np.pi * 933.82 * waist**2, rtol=1e-5)
