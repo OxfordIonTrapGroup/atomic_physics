8b62c4db8937dfaebf3e6c1ab96bd6d311cd1c2e
Thomas Harty <thomas.peter.harty@gmail.com>
2025-01-05 11:54:02 +0000

    test_atom_factory: add test on level sorting

--- atomic_physics/tests/test_atom_factory.py
+++ atomic_physics/tests/test_atom_factory.py
@@ -1,18 +1,73 @@
-# """Test Spin 1/2 nuclei"""
-# import unittest
-# from atomic_physics.ions import ca40
+import unittest
+
+import numpy as np
+
+from atomic_physics.ions import ca43
 
 
-# class TesAtomFactory(unittest.TestCase):
-#     def test_sorting(self):
-#         ion = ca40.Ca40.filter_levels(level_filter=(ca40.ground_level, ca40.shelf))(magnetic_field=100e-4)
+class TestAtomFactory(unittest.TestCase):
+    """Tests for :class:`atomic_physics.core.AtomFactory`."""
 
-#         # check that states with indices 0 and 1 belong to S1/2
-#         for i in range(2):
-#             self.assertEqual(ion.get_level_for_state(i).L, 0)
-#             self.assertEqual(ion.get_level_for_state(i).J, 0.5)
+    def test_sorting(self):
+        """
+        Test that the atom factory sorts the levels into energy ordering correctly.
+        """
+        ion = ca43.Ca43(magnetic_field=1.0)
+        levels_sorted = sorted(
+            ion.level_states.items(), key=lambda item: item[1].frequency
+        )
 
-#         # check that states with indices 2-7 belong to D5/2
-#         for i in range(6):
-#             self.assertEqual(ion.get_level_for_state(i + 2).L, 2)
-#             self.assertEqual(ion.get_level_for_state(i + 2).J, 2.5)
+        assert levels_sorted[0][0] == ca43.S12
+        assert levels_sorted[1][0] == ca43.D32
+        assert levels_sorted[2][0] == ca43.D52
+        assert levels_sorted[3][0] == ca43.P12
+        assert levels_sorted[4][0] == ca43.P32
+
+        # check all levels have the right energy
+        assert levels_sorted[0][1].frequency == 0  # S 1/2 is the ground level
+        np.testing.assert_allclose(
+            levels_sorted[3][1].frequency - levels_sorted[1][1].frequency,
+            ion.transitions["866"].frequency,
+            rtol=1e-3,
+        )
+        np.testing.assert_allclose(
+            levels_sorted[1][1].frequency, ion.transitions["733"].frequency, rtol=1e-3
+        )
+        np.testing.assert_allclose(
+            levels_sorted[2][1].frequency, ion.transitions["729"].frequency, rtol=1e-3
+        )
+        np.testing.assert_allclose(
+            levels_sorted[3][1].frequency, ion.transitions["397"].frequency, rtol=1e-3
+        )
+        np.testing.assert_allclose(
+            levels_sorted[4][1].frequency, ion.transitions["393"].frequency, rtol=1e-3
+        )
+
+        I_dim = 2 * ion.nuclear_spin + 1
+        for level, states in ion.level_states.items():
+            J_dim = 2 * level.J + 1
+            assert states.num_states == np.rint(I_dim * J_dim)
+            assert states.stop_index == states.start_index + states.num_states
+
+        # states should be in reverse energy order
+        assert ion.level_states[ca43.P32].start_index == 0
+        assert (
+            ion.level_states[ca43.P12].start_index
+            == ion.level_states[ca43.P32].stop_index
+        )
+        assert (
+            ion.level_states[ca43.D52].start_index
+            == ion.level_states[ca43.P12].stop_index
+        )
+        assert (
+            ion.level_states[ca43.D32].start_index
+            == ion.level_states[ca43.D52].stop_index
+        )
+        assert (
+            ion.level_states[ca43.S12].start_index
+            == ion.level_states[ca43.D32].stop_index
+        )
+
+    def test_filtering(self):
+        """Test that the level filtering works correctly."""
+        pass
