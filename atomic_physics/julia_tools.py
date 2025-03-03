1b308a82220ea6e06abb1f5ced5206c28c5f8131
Thomas Harty <thomas.peter.harty@gmail.com>
2025-03-03 23:04:43 +0000

    julia_tools: add fmt-test

--- atomic_physics/julia_tools.py
+++ atomic_physics/julia_tools.py
@@ -1,6 +1,15 @@
+import sys
 from juliacall import Main as jl
 
 
 def fmt():
     jl.seval("using JuliaFormatter")
     jl.seval('format(".")')
+
+
+def fmt_test():
+    jl.seval("using JuliaFormatter")
+    ret = jl.seval('format(".")')
+
+    if not ret:
+        sys.exit(1)
