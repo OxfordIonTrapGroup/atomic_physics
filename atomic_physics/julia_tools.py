60e9614247c69eaf879a98286a3f4ba0fe5d89eb
Thomas Harty <thomas.peter.harty@gmail.com>
2025-03-03 23:33:38 +0000

    fmt

--- atomic_physics/julia_tools.py
+++ atomic_physics/julia_tools.py
@@ -1,15 +1,18 @@
 import sys
-from juliacall import Main as jl
 
+try:
+    from juliacall import Main as jl
 
-def fmt():
-    jl.seval("using JuliaFormatter")
-    jl.seval('format(".")')
+    def fmt():
+        jl.seval("using JuliaFormatter")
+        jl.seval('format(".")')
 
+    def fmt_test():
+        jl.seval("using JuliaFormatter")
+        ret = jl.seval('format(".")')
 
-def fmt_test():
-    jl.seval("using JuliaFormatter")
-    ret = jl.seval('format(".")')
+        if not ret:
+            sys.exit(1)
 
-    if not ret:
-        sys.exit(1)
+except ModuleNotFoundError:
+    pass
