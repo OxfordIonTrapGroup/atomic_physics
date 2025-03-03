f39bf141bf5a0aa1aceb968ddc8ffb6ad113fd13
Thomas Harty <thomas.peter.harty@gmail.com>
2025-03-03 22:39:22 +0000

    Bloch: move julia code into its own module

--- atomic_physics/bloch.py
+++ atomic_physics/bloch.py
@@ -18,35 +18,8 @@ except ModuleNotFoundError:
 
 if HAS_JULIA:
     jl = juliacall.newmodule("Bloch")
-    jl.seval(
-        """
-        using QuantumOptics
-        using PythonCall
-
-        function make_hamiltonian(
-            num_states::Int64,
-            rabi_matrix::Matrix{ComplexF64},
-            detuning_matrix::Matrix{Float64}
-        )
-            basis = NLevelBasis(num_states)
-
-            elements = findall(x -> x != 0., rabi_matrix)
-            hamiltonians = Vector{QuantumOpticsBase.TimeDependentSum}(undef, length(elements))
-
-            for (hamiltonian_index, state_index) in enumerate(elements)
-                rabi = rabi_matrix[state_index]
-                detuning = detuning_matrix[state_index]
-
-                sigma_plus = transition(basis, state_index[1], state_index[2])
-
-                hamiltonian_static = 1 / 2 * rabi * sigma_plus
-                hamiltonian_half = TimeDependentSum((t->exp(-1im*detuning*t))=>hamiltonian_static)
-                hamiltonians[hamiltonian_index] = hamiltonian_half + dagger(hamiltonian_half)
-            end
-            return sum(hamiltonians)
-        end
-        """
-    )
+    jl.seval("using QuantumOptics")
+    jl.seval("using bloch")
 
 
 class Bloch:
