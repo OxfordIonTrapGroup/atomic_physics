7e463c0443dbf224a0863824e09035d860083634
Thomas Harty <thomas.peter.harty@gmail.com>
2025-03-03 23:23:26 +0000

    fmt-jl

--- atomic_physics/bloch/src/bloch.jl
+++ atomic_physics/bloch/src/bloch.jl
@@ -7,11 +7,11 @@ using QuantumOptics
 function make_hamiltonian(
     num_states::Int64,
     rabi_matrix::Matrix{ComplexF64},
-    detuning_matrix::Matrix{Float64}
+    detuning_matrix::Matrix{Float64},
 )
     basis = NLevelBasis(num_states)
 
-    elements = findall(x -> x != 0., rabi_matrix)
+    elements = findall(x -> x != 0.0, rabi_matrix)
     hamiltonians = Vector{QuantumOpticsBase.TimeDependentSum}(undef, length(elements))
 
     for (hamiltonian_index, state_index) in enumerate(elements)
@@ -21,7 +21,8 @@ function make_hamiltonian(
         sigma_plus = transition(basis, state_index[1], state_index[2])
 
         hamiltonian_static = 1 / 2 * rabi * sigma_plus
-        hamiltonian_half = TimeDependentSum((t->exp(-1im*detuning*t))=>hamiltonian_static)
+        hamiltonian_half =
+            TimeDependentSum((t -> exp(-1im * detuning * t)) => hamiltonian_static)
         hamiltonians[hamiltonian_index] = hamiltonian_half + dagger(hamiltonian_half)
     end
     return sum(hamiltonians)
